9f57c5da1d90ef5a21f60b0be30cb900040121ac
Obed Sarpong <obed5@szut.uni-bremen.de>
2018-10-26 14:50:08 +0200

    Erster Commit

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+
+public class Test {
+	public static void main(String[] args) {
+		
+		String a = "Stay High ist das Label ja werde mich nie ändern ja THC Gangmember Ja"
+				+ "Top 10 in drei Länder ja";
+	}
+
+}
